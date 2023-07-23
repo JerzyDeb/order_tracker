a3536ed803d5e589d19d21ce2c1854a84d77b8d4
jerzy <jurasafc@gmail.com>
2023-07-23 23:05:10 +0200

    fixes

---
+++ api/views.py
@@ -0,0 +1,24 @@
+"""Api views."""
+
+
+# Django
+from django.conf import settings
+
+# 3rd-party
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt import authentication
+
+# Project
+from .utils import get_serializer_class
+from .utils import get_model_objects
+
+
+class OrderList(ListAPIView):  # noqa: D101
+    authentication_classes = [authentication.JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = get_serializer_class()
+
+    def get_queryset(self):
+        user = self.request.user
+        return get_model_objects(user)
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ordertracker',
+    name='order_tracker',
     version='0.1',
     packages=find_packages(),
     install_requires=[
         'Django',
         'djangorestframework',
         'djangorestframework-simplejwt',
-        'drf-yasg==1.21.7',
+        'drf-yasg',
     ],
 )
